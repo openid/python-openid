8fd7ddc5f7ff9cee349792e0c15992d8cbd0210c
tailor <chowells@janrain.com>
2005-06-10 20:34:53 +0000

    [project @ Added a power_mod function for effient dh calculation]

--- dh.py
+++ dh.py
@@ -8,3 +8,15 @@ def encode_long(l):
 
 def decode_long(s):
    return pickle.decode_long(''.join(reversed(s)))
+
+def power_mod(base, exp, modulus):
+    square = base % modulus
+    result = 1
+    while exp > 0:
+        if exp & 1 == 1:
+            result = (result * square) % modulus
+        exp /= 2
+        
+        square = (square * square) % modulus
+    
+    return result
