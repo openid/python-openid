43769b6faa0c50e1e70244fecd92d1ef838044bf
tailor <chowells@janrain.com>
2005-06-15 00:05:41 +0000

    [project @ Moved some functionality to a utility module]

--- server.py
+++ server.py
@@ -1,56 +1,16 @@
-import datetime
-import pickle
-import binascii
-import sha
+from util import (sha1, long2a, a2long, w3cdate, to_b64, from_b64,
+                  kvform, kvform2, strxor)
 
 _secret_sizes = {
     'HMAC-SHA1': 20,
     }
 
-def _sha1(s):
-    return sha.new(s).digest()
-
-def _long2a(l):
-    if l == 0:
-        return '\x00'
-    
-    return ''.join(reversed(pickle.encode_long(l)))
-
-def _a2long(s):
-    return pickle.decode_long(''.join(reversed(s)))
-
-def _w3cdate(x):
-    """Represent UNIX time x as a W3C UTC timestamp"""
-    dt = datetime.datetime.utcfromtimestamp(x)
-    dt = dt.replace(microsecond=0)
-    return dt.isoformat() + 'Z'
-
-def _to_b64(s):
-    """Represent string s as base64, omitting newlines"""
-    return binascii.b2a_base64(s)[:-1]
-
-def _from_b64(s):
-    return binascii.a2b_base64(s)
-
-def _kvform2(d):
-    """Represent dict d as newline-terminated key:value pairs; return
-    also order of keys"""
-    keys = d.keys()
-    return keys, ''.join(['%s:%s\n' % (k, d[k]) for k in keys])
-
-def _kvform(d):
-    "Represent dict d as newline-terminated key:value pairs"
-    return _kvform2(d)[1]
-
-def _strxor(aa, bb):
-    return "".join([chr(ord(a) ^ ord(b)) for a, b in zip(aa, bb)])
-
 _default_modulus = 155172898181473697471232257763715539915724801966915404479707795314057629378541917580651227423698188993727816152646631438561595825688188889951272158842675419950341258706556549803580104870537681476726513255747040765857479291291572334510643245094715007229621094194349783925984760375594985848253359305585439638443L
 
 _default_gen = 2
 
-_enc_default_modulus = _to_b64(_long2a(_default_modulus))
-_enc_default_gen = _to_b64(_long2a(_default_gen))
+_enc_default_modulus = to_b64(long2a(_default_modulus))
+_enc_default_gen = to_b64(long2a(_default_gen))
 
 class OpenIDServer(object):
     def __init__(self, srand=None):
@@ -81,20 +41,19 @@ class OpenIDServer(object):
             if session_type == 'DH-SHA1':
                 enc_dh_mod = args.pop('openid.dh_modulus', _enc_default_modulus)
                 enc_dh_gen = args.pop('openid.dh_gen', _enc_default_gen)
-                dh_modulus = _a2long(_from_b64(enc_dh_mod))
-                dh_gen = _a2long(_from_b64(enc_dh_gen))
+                dh_modulus = a2long(from_b64(enc_dh_mod))
+                dh_gen = a2long(from_b64(enc_dh_gen))
 
                 enc_dh_cons_pub = args.pop('openid.dh_consumer_public')
-                dh_cons_pub = _a2long(_from_b64(enc_dh_cons_pub))
+                dh_cons_pub = a2long(from_b64(enc_dh_cons_pub))
 
                 dh_server_private = self.srand.randrange(1, dh_modulus - 1)
                 dh_server_public = pow(dh_gen, dh_server_private, dh_modulus)
-                enc_dh_server_public = _to_b64(_long2a(dh_server_public))
+                enc_dh_server_public = to_b64(long2a(dh_server_public))
 
                 dh_shared = pow(dh_cons_pub, dh_server_private, dh_modulus)
                 
-                enc_mac_key = _to_b64(
-                    _strxor(secret, _sha1(_long2a(dh_shared))))
+                enc_mac_key = to_b64(strxor(secret, sha1(long2a(dh_shared))))
 
                 reply.update({
                     'session_type': session_type,
@@ -104,21 +63,21 @@ class OpenIDServer(object):
             else:
                 raise NotImplementedError
         else:
-            reply['openid.mac_key'] = _to_b64(secret)
+            reply['openid.mac_key'] = to_b64(secret)
 
         reply.update({
             'assoc_type': assoc_type,
             'handle': handle,
-            'issued': _w3cdate(issued),
-            'replace_after': _w3cdate(replace_after),
-            'expiry': _w3cdate(expiry),
+            'issued': w3cdate(issued),
+            'replace_after': w3cdate(replace_after),
+            'expiry': w3cdate(expiry),
             })
         
         if len(args) > 0:
-            # this is a bit strange...  but letting it go for now
+            # this might be a bit strange, but ignoring for the moment
             pass
 
-        return False, _kvform(reply)
+        return False, kvform(reply)
 
     def getNewSecret(self, size):
         """Returns a tuple (handle, secret) for an association with a
---
+++ util.py
@@ -0,0 +1,43 @@
+import datetime
+import pickle
+import binascii
+import sha
+
+def sha1(s):
+    return sha.new(s).digest()
+
+def long2a(l):
+    if l == 0:
+        return '\x00'
+    
+    return ''.join(reversed(pickle.encode_long(l)))
+
+def a2long(s):
+    return pickle.decode_long(''.join(reversed(s)))
+
+def w3cdate(x):
+    """Represent UNIX time x as a W3C UTC timestamp"""
+    dt = datetime.datetime.utcfromtimestamp(x)
+    dt = dt.replace(microsecond=0)
+    return dt.isoformat() + 'Z'
+
+def to_b64(s):
+    """Represent string s as base64, omitting newlines"""
+    return binascii.b2a_base64(s)[:-1]
+
+def from_b64(s):
+    return binascii.a2b_base64(s)
+
+def kvform2(d):
+    """Represent dict d as newline-terminated key:value pairs; return
+    also order of keys"""
+    keys = d.keys()
+    return keys, ''.join(['%s:%s\n' % (k, d[k]) for k in keys])
+
+def kvform(d):
+    "Represent dict d as newline-terminated key:value pairs"
+    return kvform2(d)[1]
+
+def strxor(aa, bb):
+    return "".join([chr(ord(a) ^ ord(b)) for a, b in zip(aa, bb)])
+
