1815fd64797f857a9cbcd14e16ab62e43bf05841
Josh Hoyt <josh@janrain.com>
2005-06-17 23:40:18 +0000

    [project @ Fix concrete server implementation to match refactored abstract server]

--- concreteserver.py
+++ concreteserver.py
@@ -1,32 +1,31 @@
 import random, time
 
+from openid.errors import ProtocolError
 from openid.server import OpenIDServer
 
-class ConcreteLinuxServer(OpenIDServer):
+class ConcreteServer(OpenIDServer):
     def __init__(self):
         OpenIDServer.__init__(self, random.SystemRandom())
         self.secret = '12345678900987654321'
-        self._handle = 'huh' # XXX: we already have a method called handle
+        self.assoc_handle = 'huh'
         self.issued = time.time()
         self.replace_after = self.issued + (60 * 60 * 24 * 29) 
         self.expiry = self.replace_after + (60 * 60 * 24)
 
-    def get_new_secret(self, size):
-        assert size == 20
-        return (self.secret, self._handle, self.issued,
+    def get_new_secret(self):
+        return (self.secret, self.assoc_handle, self.issued,
                 self.replace_after, self.expiry)
 
-    def get_secret(self, assoc_handle):
-        if assoc_handle == self._handle:
+    def lookup_secret(self, assoc_handle):
+        if assoc_handle == self.assoc_handle:
             return self.secret, self.expiry 
         else:
-            return None
+            raise ProtocolError('Unknown assoc_handle: %r' % assoc_handle)
 
-    def get_server_secret(self, size):
-        # XXX: do something with size
-        return self.secret, self._handle, self.issued, self.expiry
+    def get_server_secret(self):
+        return self.secret, self.assoc_handle
 
-    def id_allows_authentication(self, identity, trust_root):
+    def get_auth_range(self, unused_identity, unused_trust_root):
         "Every identity trusts every trust_root!  Yay!"
         now = time.time()
         in_an_hour = now + (60 * 60)
