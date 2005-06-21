2cd4e5fffe89c0f2abe2a5205a49468ffd7b3889
tailor <chowells@janrain.com>
2005-06-21 20:19:59 +0000

    [project @ Make sampleserver actually generate ID pages]

--- examples/sampleserver.py
+++ examples/sampleserver.py
@@ -5,7 +5,7 @@ import time, random
 from openid.examples import util
 from openid.errors import ProtocolError
 from openid.server import OpenIDServer
-from openid.interface import Request
+from openid.interface import Request, response_page
 
 class ConcreteServer(OpenIDServer):
     def __init__(self):
@@ -39,21 +39,41 @@ class ConcreteServer(OpenIDServer):
         return now, in_an_hour
 
     def get_lifetime(self, identity):
-        return 500
+        return 60 * 60
+
+    def get_user_setup_url(self, identity, trust_root):
+        return ''
+
+    def get_setup_response(self, identity, trust_root, return_to):
+        return response_page('')
 
 
-_server = None
-def getServer():
-    global _server
-    if _server is None:
-        _server = ConcreteServer()
-    return _server
+server = ConcreteServer()
+identitypage = """<html>
+<head>
+  <title>This is an identity page</title>
+  <link rel="openid.server" href="http://localhost:8082/?action=openid">
+</head>
+<body>
+  <p>This is an identity page for %r.</p>
+</body>
+</html>
+"""
+
+mainpage = """<html>
+<head>
+  <title>Simple OpenID server</title>
+</head>
+<body>
+<p>This is a simple OpenID server</p>
+</body>
+</html>
+"""
 
 class ServerHandler(util.HTTPHandler):
 
     def handleOpenIDRequest(self, req):
         try:
-            server = getServer()
             response = server.handle(req)
             if response.code == 302:
                 self._redirect(response.redirect_url)
@@ -69,16 +89,30 @@ class ServerHandler(util.HTTPHandler):
 
     def do_GET(self):
         print 'IN GET'
-        qs = urlparse(self.path)[4]
-        query = util.parseQuery(qs)
-        self.handleOpenIDRequest(Request(query, 'GET'))
+        parsed = urlparse(self.path)
+        query = util.parseQuery(parsed[4])
+        if query.get('action') == 'openid':
+            self.handleOpenIDRequest(Request(query, 'GET'))
+        elif len(query) == 0:
+            path = parsed[2]
+            if path == '/':
+                self._headers()
+                self.wfile.write(mainpage)
+            else:
+                self._headers()
+                self.wfile.write(identitypage % path[1:])
+        else:
+            self._headers(500)
 
     def do_POST(self):
         # post data is urlencoded args
         content_length = int(self.headers['Content-Length'])
         post_data = self.rfile.read(content_length)
         query = util.parseQuery(post_data)
-        self.handleOpenIDRequest(Request(query, 'POST'))
+        if query.get('action') == 'openid':
+            self.handleOpenIDRequest(Request(query, 'POST'))
+        else:
+            self._headers(500)
 
 
 if __name__ == '__main__':
