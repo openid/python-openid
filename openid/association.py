513664cae1be0199b39aa3119dfd193afe949ee1
tailor <chowells@janrain.com>
2005-07-05 17:16:56 +0000

    [project @ Make DiffieHelmanAssociator use cryptographic-quality source of randomness]

--- openid/association.py
+++ openid/association.py
@@ -109,6 +109,7 @@ class BaseAssociationManager(DumbAssociationManager):
 class DiffieHelmanAssociator(object):
     def __init__(self, http_client):
         self.http_client = http_client
+        self.rand = random.SystemRandom()
 
     def get_mod_gen(self):
         """-> (modulus, generator) for Diffie-Helman
@@ -118,7 +119,7 @@ class DiffieHelmanAssociator(object):
 
     def associate(self, server_url):
         p, g = self.get_mod_gen()
-        priv_key = random.randrange(1, p-1)
+        priv_key = self.rand.randrange(1, p-1)
 
         args = {
             'openid.mode': 'associate',
