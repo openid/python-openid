3e224b8b267de4c735e655f311fe6abf14c6d84d
tailor <chowells@janrain.com>
2005-10-07 00:04:16 +0000

    [project @ doc project continuing]

--- openid/consumer/interface.py
+++ openid/consumer/interface.py
@@ -2,101 +2,110 @@
 This module is intended to document the main interface with the OpenID
 consumer libary.  The only part of the library which has to be used
 and isn't documented in full here is the store required to create an
-OpenIDConsumer instance.  More on the abstract store type and
+C{OpenIDConsumer instance}.  More on the abstract store type and
 concrete implementations of it that are provided in the documentation
-for the __init__ method of the OpenIDConsumer class.
+for the C{__init__} method of the C{OpenIDConsumer} class.
 
 
-== OVERVIEW ==
+OVERVIEW
+========
 
-The OpenID identity verification process most commonly uses the
-following steps, as visible to the user of this library:
+    The OpenID identity verification process most commonly uses the
+    following steps, as visible to the user of this library:
 
-1. The user enters their OpenID into a field on the consumer's site,
-   and hits some sort of log in button.
+    1. The user enters their OpenID into a field on the consumer's
+       site, and hits some sort of log in button.
 
-2. The consumer site sends the browser a redirect, sending the browser
-   to the identity server's site.
+    2. The consumer site sends the browser a redirect, sending the
+       browser to the identity server's site.
 
-3. The identity server's site sends the browser a redirect, sending
-   the browser back to the consumer's site with the information
-   necessary to confirm the user's identity.
+    3. The identity server's site sends the browser a redirect,
+       sending the browser back to the consumer's site with the
+       information necessary to confirm the user's identity.
 
-There are a lot of conditional extras in the process, but that is the
-basic flow of an OpenID login from the consumer's point of view.  The
-most important part of that flow is noting that the consumer's site
-must handle two separate HTTP requests in order to perform the full
-identity check.
-
-
-== LIBRARY DESIGN ==
-
-This consumer library is designed with that flow in mind.  Our goal is
-to make it as easy as possible to perform the above steps securely.
-
-*************  Stuff that may depend on what Josh is doing....
+    There are a lot of conditional extras in the process, but that is
+    the basic flow of an OpenID login from the consumer's point of
+    view.  The most important part of the flow to note is the
+    consumer's site must handle two separate HTTP requests in order to
+    perform the full identity check.
 
 
-== STORES AND DUMB MODE ==
+LIBRARY DESIGN
+==============
 
-OpenID is a protocol that works best when the consumer site is able to
-store some state.  This is the normal mode of operation for the
-protocol, and is sometimes referred to as smart mode.  There is also a
-fallback mode, known as dumb mode, which is available when the
-consumer site is not able to store state.  This mode should be avoided
-when possible, as it leaves the implementation more vulnerable to
-replay attacks.
+    This consumer library is designed with that flow in mind.  Our
+    goal is to make it as easy as possible to perform the above steps
+    securely.
 
-The mode the library works in for normal operation* is determined by
-the store that it is given.  The store is an abstraction that handles
-the data that the consumer needs to manage between http requests in
-order to operate efficiently and securely.
-
-Several store implementation are provided, and the interface is fully
-documented so that custom stores can be used as well.  See the
-documentation for the OpenIDConsumer class for more information
-on the interface for stores.  The concrete implementations that are
-provided allow the consumer site to store the necessary data in
-several different ways: in the filesystem, in a MySQL database, or in
-an SQLite database.
-
-There is an additional concrete store provided that puts the system in
-dumb mode.  This is not recommended, as it removes the library's
-ability to stop replay attacks reliably.  It still uses time-based
-checking to make replay attacks only possible within a small window,
-but they remain possible within that window.  This store should only
-be used if the consumer site has no way to store data between requests
-at all.
-
-*: There are fallback cases in the protocol, where even a consumer
-usually running in smart mode acts like it's in dumb mode for one
-request, but those cases are not the normal operation.  Additionally,
-the fallback cases are much more secure than pure dumb mode, as they
-still are making use the consumer's ability to store state.
+    Waiting on resolution of structure.
 
 
-== IMMEDIATE MODE ==
+STORES AND DUMB MODE
+====================
 
-In the flow described above, there's a step which may occur if the
-user needs to confirm to the identity server that it's ok to authorize
-his or her identity.  The server may draw pages asking for information
-from the user before it redirects the browser back to the consumer's
-site.  This is generally transparent to the consumer site, so it is
-typically ignored as an implementation detail.
+    OpenID is a protocol that works best when the consumer site is
+    able to store some state.  This is the normal mode of operation
+    for the protocol, and is sometimes referred to as smart mode.
+    There is also a fallback mode, known as dumb mode, which is
+    available when the consumer site is not able to store state.  This
+    mode should be avoided when possible, as it leaves the
+    implementation more vulnerable to replay attacks.
 
-There can be times, however, where the consumer site wants to get a
-response immediately.  When this is the case, the consumer can put the
-library in immediate mode.  In immediate mode, there is an extra
-response possible from the server, which is essentially the server
-reporting that it doesn't have enough information to answer the
-question yet.  In addition to saying that, the identity server
-provides a URL to which the user can be sent to provide the needed
-information and let the server finish handling the original request.
+    The mode the library works in for normal operation* is determined
+    by the store that it is given.  The store is an abstraction that
+    handles the data that the consumer needs to manage between http
+    requests in order to operate efficiently and securely.
+
+    Several store implementation are provided, and the interface is
+    fully documented so that custom stores can be used as well.  See
+    the documentation for the C{OpenIDConsumer} class for more
+    information on the interface for stores.  The concrete
+    implementations that are provided allow the consumer site to store
+    the necessary data in several different ways: in the filesystem,
+    in a MySQL database, or in an SQLite database.
+
+    There is an additional concrete store provided that puts the
+    system in dumb mode.  This is not recommended, as it removes the
+    library's ability to stop replay attacks reliably.  It still uses
+    time-based checking to make replay attacks only possible within a
+    small window, but they remain possible within that window.  This
+    store should only be used if the consumer site has no way to store
+    data between requests at all.
+
+    *: There are fallback cases in the protocol, where even a consumer
+    usually running in smart mode acts like it's in dumb mode for one
+    request, but those cases are not the normal operation.
+    Additionally, the fallback cases are much more secure than pure
+    dumb mode, as they still are making use the consumer's ability to
+    store state.
 
 
-== USING THIS LIBRARY ==
+IMMEDIATE MODE
+==============
 
-*********  Waiting on resolution of structure
+    In the flow described above, there's a step which may occur if the
+    user needs to confirm to the identity server that it's ok to
+    authorize his or her identity.  The server may draw pages asking
+    for information from the user before it redirects the browser back
+    to the consumer's site.  This is generally transparent to the
+    consumer site, so it is typically ignored as an implementation
+    detail.
+
+    There can be times, however, where the consumer site wants to get
+    a response immediately.  When this is the case, the consumer can
+    put the library in immediate mode.  In immediate mode, there is an
+    extra response possible from the server, which is essentially the
+    server reporting that it doesn't have enough information to answer
+    the question yet.  In addition to saying that, the identity server
+    provides a URL to which the user can be sent to provide the needed
+    information and let the server finish handling the original
+    request.
+
+
+USING THIS LIBRARY
+==================
+
+    Waiting on resolution of structure.
 """
 
 class OpenIDConsumer(object):
@@ -115,46 +124,44 @@ class OpenIDConsumer(object):
         memory between requests to the server.
 
 
-        ARGUMENTS:
+        @param store: This must be an object that implements the
+            interface in C{openid.consumer.stores.OpenIDStore}.
+            Several concrete implementations are provided, to cover
+            most common use cases.  For stores backed by MySQL or
+            SQLite, see the openid.consumer.sqlstore package.  For a
+            filesystem-backed store, see the
+            C{openid.consumer.filestore} package.
 
-        *store* - This must be an object that implements the interface
-        in openid.consumer.stores.OpenIDStore.  Several concrete
-        implementations are provided, to cover most common use cases.
-        For stores backed by MySQL or SQLite, see the
-        openid.consumer.sqlstore package.  For a filesystem-backed
-        store, see the openid.consumer.filestore package.
+            As a last resort, if it isn't possible for the server to
+            store state at all, an instance of
+            C{openid.consumer.stores.DumbStore} can be used.  This
+            should be an absolute last resort, though, as it makes the
+            consumer vulnerable to replay attacks over the lifespan of
+            the tokens the library creates.  See XXX for more
+            information on controlling the lifespan of tokens.
 
-        As a last resort, if it isn't possible for the server to store
-        state at all, an instance of openid.consumer.stores.DumbStore
-        can be used.  This should be an absolute last resort, though,
-        as it makes the consumer vulnerable to replay attacks over the
-        lifespan of the tokens the library creates.  See XXX for more
-        information on controlling the lifespan of tokens.
-
-
-        *immediate* - This is an optional boolean value.  It controls
-        whether the library uses immediate mode, as explained in the
-        module description.  The default value is False, which
-        disables immediate mode.
+        @type store: C{openid.consumer.stores.OpenIDStore}
 
 
-        *fetcher* - This is an optional instance of
-        openid.consumer.fetchers.OpenIDHTTPFetcher.  If present, the
-        provided fetcher is used by the library to fetch user's
-        identity pages and make direct requests to the identity
-        server.  If it's not present, a default fetcher is used.  The
-        default fetcher uses curl if the pycurl bindings are
-        available, and uses urllib if not.
+        @param immediate: This is an optional boolean value.  It
+            controls whether the library uses immediate mode, as
+            explained in the module description.  The default value is
+            False, which disables immediate mode.
+
+        @type immediate: C{bool}
+
+        @param fetcher: This is an optional instance of
+            C{openid.consumer.fetchers.OpenIDHTTPFetcher}.  If
+            present, the provided fetcher is used by the library to
+            fetch user's identity pages and make direct requests to
+            the identity server.  If it's not present, a default
+            fetcher is used.  The default fetcher uses curl if the
+            pycurl bindings are available, and uses urllib if not.
+
+        @type fetcher: C{openid.consumer.fetchers.OpenIDHTTPFetcher}
 
 
-        RETURNS:
-
-        As an initializer, this method has no return value.
-
-
-        EXCEPTIONS:
-
-        This method is not expected to raise any exceptions.
+        @return: As an initializer, this method has no return value.
         """
         if fetcher is None:
             from openid.consumer.fetchers import getHTTPFetcher
