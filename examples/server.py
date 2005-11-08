d871d9176a3c4a40659841e70a7e9ea26cd054d0
Josh Hoyt <josh@janrain.com>
2005-11-08 00:15:59 +0000

    [project @ Fix crash when not logged in and attempting to verify. Change some of the text. Allow logout. (in the server example)]

--- examples/server.py
+++ examples/server.py
@@ -109,8 +109,12 @@ class ServerHandler(BaseHTTPRequestHandler):
                 if 'yes' in self.query:
                     identity = self.query['identity']
                     trust_root = self.query['trust_root']
-                    self.server.approved[(identity, trust_root)] = 1
+                    if self.query.get('remember', 'no') == 'yes':
+                        duration = 'always'
+                    else:
+                        duration = 'once'
 
+                    self.server.approved[(identity, trust_root)] = duration
                     self.redirect(self.query['success_to'])
 
                 elif 'no' in self.query:
@@ -136,10 +140,17 @@ class ServerHandler(BaseHTTPRequestHandler):
         identity, trust_root = \
                   self.server.openid.getAuthenticationData(self.query)
 
+        identity_ok = self.user and identity == self.server.base_url + self.user
         # check all three important parts
-        authorized = identity and \
-                     identity == self.server.base_url + self.user and \
-                     (identity, trust_root) in self.server.approved
+        if identity_ok:
+            key = (identity, trust_root)
+            approval = self.server.approved.get(key)
+            if approval == 'once':
+                del self.server.approved[key]
+
+            authorized = approval is not None
+        else:
+            authorized = False
 
         status, info = self.server.openid.getAuthenticationResponse(\
             authorized, self.query)
@@ -150,7 +161,7 @@ class ServerHandler(BaseHTTPRequestHandler):
         elif status == interface.DO_AUTH:
             retry, cancel = info
 
-            if identity and identity == self.server.base_url + self.user:
+            if identity_ok:
                 self.showDecidePage(identity, trust_root, retry, cancel)
             else:
                 self.showLoginPage(retry, cancel)
@@ -166,7 +177,13 @@ class ServerHandler(BaseHTTPRequestHandler):
 
     def doLogin(self):
         if 'submit' in self.query:
-            cookie = ('Set-Cookie', 'user=%s' % self.query['user'])
+            if 'user' in self.query:
+                cookie = ('Set-Cookie', 'user=%s' % self.query['user'])
+            else:
+                t1970 = time.gmtime(0)
+                expires = time.strftime(
+                    'Expires=%a, %d-%b-%y %H:%M:%S GMT', t1970)
+                cookie = ('Set-Cookie', 'user=;%s' % expires)
             self.redirect(self.query['success_to'], cookie)
         elif 'cancel' in self.query:
             self.redirect(self.query['fail_to'])
@@ -232,12 +249,12 @@ class ServerHandler(BaseHTTPRequestHandler):
             'retry': retry,
             'cancel': cancel,
             }
-        self.showPage(200, 'Allow this login?', msg='''\
-        <p>A new site has asked for your identity.  If this login is
-        approved, the site represented by the Trust Root below will be
-        told that you are the identity listed below.  (If you are
+        self.showPage(200, 'Approve OpenID request?', msg='''\
+        <p>A new site has asked for your identity.  If you approve,
+        the site represented by the trust root below will be
+        told that you control identity URL listed below. (If you are
         using a delegated identity, the site will take care of
-        reversing the delegation on its own.)</p>        
+        reversing the delegation on its own.)</p>
         ''' % args, form='''\
         <table>
           <tr><td>Identity:</td><td>%(identity)s</td></tr>
@@ -245,6 +262,9 @@ class ServerHandler(BaseHTTPRequestHandler):
         </table>
         <p>Allow this login to proceed?</p>
         <form method="POST" action="/allow">
+          <input type="checkbox" id="remember" name="remember" value="yes"
+              checked="checked" /><label for="remember">Remember this
+              decision</label><br />
           <input type="hidden" name="identity" value="%(identity)s" />
           <input type="hidden" name="trust_root" value="%(trust_root)s" />
           <input type="hidden" name="success_to" value="%(retry)s" />
@@ -259,33 +279,81 @@ class ServerHandler(BaseHTTPRequestHandler):
               self.server.base_url
         ident = self.server.base_url + path[1:]
 
+        approved_trust_roots = []
+        for (aident, trust_root) in self.server.approved.keys():
+            if aident == ident:
+                trs = '<li><tt>%s</tt></li>\n' % escape(trust_root)
+                approved_trust_roots.append(trs)
+
+        if approved_trust_roots:
+            prepend = '<p>Approved trust roots:</p>\n<ul>\n'
+            approved_trust_roots.insert(0, prepend)
+            approved_trust_roots.append('</ul>\n')
+            msg = ''.join(approved_trust_roots)
+        else:
+            msg = ''
+
         self.showPage(200, 'An Identity Page', link_tag=tag, msg='''\
-        <p>This is an identity page for %s.  Not much to see here.</p>
-        <p><a href="/">Main Page</a></p>
-        ''' % ident)
+        <p>This is an identity page for %s.</p>
+        %s
+        ''' % (ident, msg))
 
     def showMainPage(self):
+        if self.user:
+            openid_url = self.server.base_url + self.user
+            user_message = """\
+            <p>You are logged in as %s. Your OpenID identity URL is
+            <tt><a href=%s>%s</a></tt>. Enter that URL at an OpenID
+            consumer to test this server.</p>
+            """ % (self.user, quoteattr(openid_url), openid_url)
+        else:
+            user_message = """\
+            <p>This server uses a cookie to remember who you are in
+            order to simulate a standard Web user experience. You are
+            not <a href='/login'>logged in</a>.</p>"""
+
         self.showPage(200, 'Main Page', msg='''\
-        <p>This is a simple OpenID server using the python OpenID
-        server library module.  If you are not logged in, you can go
-        to the <a href="/login">login</a> page.</p>
-        ''')
+        <p>This is a simple OpenID server implemented using the <a
+        href="http://openid.schtuff.com/">Python OpenID
+        library</a>.</p>
+
+        %s
+
+        <p>To use this server with a consumer, the consumer must be
+        able to fetch HTTP pages from this web server. If this
+        computer is behind a firewall, you will not be able to use
+        OpenID consumers outside of the firewall with it.</p>
+
+        <p>The URL for this server is <a href=%s><tt>%s</tt></a>.</p>
+        ''' % (user_message, quoteattr(self.server.base_url), self.server.base_url))
 
     def showLoginPage(self, success_to, fail_to):
-        self.showPage(200, 'Login Page', msg='''\
-        <p>This is the login page.  You can reach this page in one of
-        two ways.  If you chose to manually log in, you know what to
-        do. The other way to reach this page is if you are attempting
-        to use this server to log in as an identity not currently
-        matching the identity you are currently logged in as on this
-        server.  In that case, either log in as the identity you specified or
-        cancel.</p>
+        if self.path == '/login':
+            user_message = None
+        else:
+            if self.user:
+                why = '''\
+                logged in as <strong>%s</strong>, who does not own the
+                identity URL that the consumer is attempting to
+                verify''' % (self.user,)
+            else:
+                why = 'not logged in to this server'
 
-        <p>This server does not use passwords because it is designed
-        as a sample only.  Adding in a password mechanism would have
-        made it more complicated without adding any benefit to
-        understanding.</p>
-        ''', form='''\
+            user_message = """
+            <p>This is the login page for the OpenID server.  You are
+            attempting to use this server to verify an identity URL
+            and you are %s. The server needs to know who you are
+            before you can approve the request.</p>
+
+            <p>Click <strong>Cancel</strong> to return to the OpenID
+            consumer that initiated this transaction.</p>
+            """ % (why,)
+
+        self.showPage(200, 'Login Page', msg=user_message, form='''\
+        <h2>Login</h2>
+        <p>You may log in with any name. This server does not use
+        passwords because it is just a sample of how to use the OpenID
+        library.</p>
         <form method="GET" action="/loginsubmit">
           <input type="hidden" name="success_to" value="%s" />
           <input type="hidden" name="fail_to" value="%s" />
@@ -299,9 +367,9 @@ class ServerHandler(BaseHTTPRequestHandler):
                  link_tag='', msg=None, err=None, form=None):
 
         if self.user is None:
-            user_link = '<a href="/login">not logged in<a>'
+            user_link = '<a href="/login">not logged in</a>.'
         else:
-            user_link = 'logged in as <a href="/%s">%s</a>' % \
+            user_link = 'logged in as <a href="/%s">%s</a>.<br /><a href="/loginsubmit?submit=true&success_to=/login">Log out</a>' % \
                         (self.user, self.user)
 
         body = ''
@@ -345,10 +413,19 @@ Content-type: text/html
     %(link_tag)s
   </head>
   <style type="text/css">
-      * {
-        font-family: verdana,sans-serif;
+      h1 a:link {
+          color: black;
+          text-decoration: none;
+      }
+      h1 a:visited {
+          color: black;
+          text-decoration: none;
+      }
+      h1 a:hover {
+          text-decoration: underline;
       }
       body {
+        font-family: verdana,sans-serif;
         width: 50em;
         margin: 1em;
       }
@@ -392,10 +469,10 @@ Content-type: text/html
     <table class="banner">
       <tr>
         <td class="leftbanner">
-          <h1>Sample OpenID Server</h1>
+          <h1><a href="/">Sample OpenID Server</a></h1>
         </td>
         <td class="rightbanner">
-          You are %(user_link)s.
+          You are %(user_link)s
         </td>
       </tr>
     </table>
