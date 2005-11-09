0f05935944183ca244fa41c8871a52bd0194e7d3
Josh Hoyt <josh@janrain.com>
2005-11-09 00:41:30 +0000

    [project @ Add testing for bad fetch and bad parse]

--- test/consumer.py
+++ test/consumer.py
@@ -1,70 +1,74 @@
+import urlparse
+import urllib
 import cgi
+
 from openid import cryptutil, dh, oidutil, kvform
 from openid.consumer import interface
 
-assoc_secret = 'another 20-byte key.'
-assoc_handle = 'Moustache'
+import _memstore
+
+assocs = [
+    ('another 20-byte key.', 'Snarky'),
+    ('\x00' * 20, 'Zeros'),
+    ]
+
+def parse(qs):
+    q = {}
+    for (k, v) in cgi.parse_qsl(qs):
+        assert not q.has_key(k)
+        q[k] = v
+    return q
+
+def associate(qs, assoc_secret, assoc_handle):
+    """Do the server's half of the associate call, using the given
+    secret and handle."""
+    q = parse(qs)
+    assert len(q) == 6
+    assert q['openid.mode'] == 'associate'
+    assert q['openid.assoc_type'] == 'HMAC-SHA1'
+    assert q['openid.session_type'] == 'DH-SHA1'
+    d = dh.DiffieHellman.fromBase64(
+        q['openid.dh_modulus'], q['openid.dh_gen'])
+
+    composite = cryptutil.base64ToLong(q['openid.dh_consumer_public'])
+    enc_mac_key = oidutil.toBase64(d.xorSecret(composite, assoc_secret))
+    reply_dict = {
+        'assoc_type':'HMAC-SHA1',
+        'assoc_handle':assoc_handle,
+        'expires_in':'600',
+        'session_type':'DH-SHA1',
+        'dh_server_public':cryptutil.longToBase64(d.createKeyExchange()),
+        'enc_mac_key':enc_mac_key,
+        }
+    return kvform.dictToKV(reply_dict)
 
 class TestFetcher(object):
-    def __init__(self):
-        self.get_responses = {}
-        self.post_responses = {}
+    def __init__(self, user_url, user_page, (assoc_secret, assoc_handle)):
+        self.get_responses = {user_url:(200, user_url, user_page)}
+        self.assoc_secret = assoc_secret
+        self.assoc_handle = assoc_handle
 
-    def response(self, url, data):
-        if data is None:
+    def response(self, url, body):
+        if body is None:
             return (404, url, 'Not found')
         else:
-            return (200, url, data)
+            return (200, url, body)
 
     def get(self, url):
-        return self.response(url, self.get_responses.get(url))
+        try:
+            return self.get_responses[url]
+        except KeyError:
+            return self.response(url, None)
 
     def post(self, url, body):
         if 'openid.mode=associate' in body:
-            return self._associate(url, body)
-        elif 'openid.mode=check_authentication':
-            return self._checkAuth(url, body)
+            response = associate(body, self.assoc_secret, self.assoc_handle)
+            return self.response(url, response)
         else:
             return self.response(url, None)
 
-    def _associate(self, url, body):
-        q = {}
-        for (k, v) in cgi.parse_qsl(body):
-            assert not q.has_key(k)
-            q[k] = v
-
-        assert len(q) == 6
-        assert q['openid.mode'] == 'associate'
-        assert q['openid.assoc_type'] == 'HMAC-SHA1'
-        assert q['openid.session_type'] == 'DH-SHA1'
-        d = dh.DiffieHellman.fromBase64(
-            q['openid.dh_modulus'], q['openid.dh_gen'])
-
-        composite = cryptutil.base64ToLong(q['openid.dh_consumer_public'])
-        enc_mac_key = oidutil.toBase64(d.xorSecret(composite, assoc_secret))
-        reply_dict = {
-            'assoc_type':'HMAC-SHA1',
-            'assoc_handle':assoc_handle,
-            'expires_in':'600',
-            'session_type':'DH-SHA1',
-            'dh_server_public':cryptutil.longToBase64(d.createKeyExchange()),
-            'enc_mac_key':enc_mac_key,
-            }
-        reply_body = kvform.dictToKV(reply_dict)
-        return self.response(url, reply_body)
         
-import _memstore
-
-def test():
-    print 'Testing consumer'
-    fetcher = TestFetcher()
-    store = _memstore.MemoryStore()
-
-    store.auth_key = '20-byte long secret.'
-    user_url = 'http://www.example.com/user.html'
-    consumer_url = 'http://consumer.example.com/'
-    server_url = 'http://server.example.com/'
-    user_page_pat = '''\
+user_page_pat = '''\
 <html>
   <head>
     <title>A user page</title>
@@ -76,39 +80,82 @@ def test():
 </html>
 '''
 
+def test_success():
+    store = _memstore.MemoryStore()
+
+    user_url = 'http://www.example.com/user.html'
+    consumer_url = 'http://consumer.example.com/'
+    server_url = 'http://server.example.com/'
     links = '<link rel="openid.server" href="%s" />' % (server_url,)
     user_page = user_page_pat % links
-    fetcher.get_responses[user_url] = user_page
+
+    fetcher = TestFetcher(user_url, user_page, assocs[0])
 
     consumer = interface.OpenIDConsumer(store, fetcher)
     (status, info) = consumer.beginAuth(user_url)
     assert status == interface.SUCCESS, status
+
     return_to = consumer_url
     trust_root = consumer_url
     redirect_url = consumer.constructRedirect(info, return_to, trust_root)
-    assert redirect_url == (
-        'http://server.example.com/?'
-        'openid.mode=checkid_setup&'
-        'openid.identity=http%3A%2F%2Fwww.example.com%2Fuser.html&'
-        'openid.trust_root=http%3A%2F%2Fconsumer.example.com%2F&'
-        'openid.assoc_handle=Moustache&'
-        'openid.return_to=http%3A%2F%2Fconsumer.example.com%2F'
-        )
+
+    parsed = urlparse.urlparse(redirect_url)
+    qs = parsed[4]
+    q = parse(qs)
+    assert q == {
+        'openid.mode':'checkid_setup',
+        'openid.identity':user_url,
+        'openid.trust_root':trust_root,
+        'openid.assoc_handle':fetcher.assoc_handle,
+        'openid.return_to':return_to,
+        }, q
+
+    assert redirect_url.startswith(server_url)
 
     query = {
         'openid.mode':'id_res',
         'openid.return_to':return_to,
         'openid.identity':user_url,
-        'openid.assoc_handle':assoc_handle,
+        'openid.assoc_handle':fetcher.assoc_handle,
         }
-    assoc = consumer.impl.store.getAssociation(server_url, assoc_handle)
-    fields = ['mode', 'return_to', 'identity']
-    sig = assoc.signDict(fields, query)
-    query['openid.signed'] = ','.join(fields)
-    query['openid.sig'] = sig
+
+    assoc = store.getAssociation(server_url, fetcher.assoc_handle)
+    assoc.addSignature(['mode', 'return_to', 'identity'], query)
+
     (status, info) = consumer.completeAuth(info.token, query)
     assert status == 'success'
     assert info == user_url
+
+def test_bad_fetch():
+    store = _memstore.MemoryStore()
+    fetcher = TestFetcher(None, None, (None, None))
+    consumer = interface.OpenIDConsumer(store, fetcher)
+    cases = [
+        (None, 'http://network.error/'),
+        (404, 'http://not.found/'),
+        (400, 'http://bad.request/'),
+        (500, 'http://server.error/'),
+        ]
+    for error_code, url in cases:
+        fetcher.get_responses[url] = (error_code, url, None)
+        (status, info) = consumer.beginAuth(url)
+        assert status == interface.HTTP_FAILURE, status
+        assert info == error_code, (url, info)
+
+def test_bad_parse():
+    store = _memstore.MemoryStore()
+    user_url = 'http://user.example.com/'
+    user_page = ''
+    fetcher = TestFetcher(user_url, user_page, (None, None))
+    consumer = interface.OpenIDConsumer(store, fetcher)
+    status, info = consumer.beginAuth(user_url)
+    assert status == interface.PARSE_ERROR
+    assert info is None
+
+def test():
+    test_success()
+    test_bad_fetch()
+    test_bad_parse()
 
 if __name__ == '__main__':
     test()
