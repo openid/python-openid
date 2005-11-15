7b3f6777a7c84a98b6cf962dea431af220bb5cee
tailor <chowells@janrain.com>
2005-11-15 19:07:10 +0000

    [project @ Add distutils support]

---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+try:
+    f = open('COPYING', 'r')
+    copying = f.read()
+finally:
+    f.close()
+
+setup(
+    name='python-openid',
+    version='1.0-beta1',
+    description='Python OpenID Library',
+    url='http://openid.schtuff.com',
+    packages=['openid',
+              'openid.consumer',
+              'openid.server',
+              'openid.stores',
+              ],
+    license=copying,
+    )
+
