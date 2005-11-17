c3f39858c3fe083fba3709916162feefbb56fa06
tailor <chowells@janrain.com>
2005-11-17 18:58:21 +0000

    [project @ Correct project url in setup.py]

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     name='python-openid',
     version='1.0-beta1',
     description='Python OpenID Library',
-    url='http://openid.schtuff.com',
+    url='http://openid.schtuff.com/',
     packages=['openid',
               'openid.consumer',
               'openid.server',
