28d24b6e5830c91a4934850ed51d644fcab8a5c6
tailor <chowells@janrain.com>
2005-11-23 01:12:29 +0000

    [project @ Move server implementation to server.py]

--- openid/server/server.py
+++ openid/server/server.py
@@ -184,6 +184,17 @@ USING THIS LIBRARY
 @sort: OK, ERROR, REDIRECT, DO_AUTH, DO_ABOUT, OpenIDServer
 """
 
+import time
+
+from openid import cryptutil
+from openid import kvform
+from openid import oidutil
+from openid.dh import DiffieHellman
+from openid.server.trustroot import TrustRoot
+from openid.association import Association
+
+_signed_fields = ['mode', 'identity', 'return_to']
+
 REDIRECT = 'redirect'
 DO_AUTH  = 'do_auth'
 DO_ABOUT = 'do_about'
@@ -197,9 +208,18 @@ class OpenIDServer(object):
     contain no per-request state, so a single instance can be reused
     (or even used concurrently by multiple threads) as needed.
 
+
+    @cvar SECRET_LIFETIME: This is the lifetime that secrets generated
+        by this library are valid for, in seconds.
+
+    @type SECRET_LIFETIME: C{int}
+
+
     @sort: __init__, getAuthenticationData, getAuthenticationResponse,
         processPost
     """
+
+    SECRET_LIFETIME = 14 * 24 * 60 * 60 # 14 days, in seconds
 
     def __init__(self, server_url, store):
         """
@@ -227,8 +247,14 @@ class OpenIDServer(object):
         @type store: An object implementing the
             C{L{openid.store.interface.OpenIDStore}} interface
         """
-        from openid.server.impl import OpenIDServerImpl
-        self.impl = OpenIDServerImpl(server_url, store)
+        self.url = server_url
+        self.normal = server_url + '|normal'
+        self.dumb = server_url + '|dumb'
+
+        if store.isDumb():
+            raise ValueError, 'OpenIDServer cannot use a dumb store.'
+
+        self.store = store
 
     def getAuthenticationData(self, args):
         """
@@ -256,7 +282,9 @@ class OpenIDServer(object):
         @rtype: (C{str} or C{unicode} or C{NoneType}, C{str} or
             C{unicode} or C{NoneType})
         """
-        return self.impl.getAuthenticationData(args)
+        trust_root = args.get('openid.trust_root')
+        identity = args.get('openid.identity')
+        return identity, trust_root
     
     def getAuthenticationResponse(self, authorized, args):
         """
@@ -343,7 +371,70 @@ class OpenIDServer(object):
 
         @rtype: (C{str}, C{str} or C{(str, str)} or C{NoneType})
         """
-        return self.impl.getAuthenticationResponse(authorized, args)
+        identity = args.get('openid.identity')
+        if identity is None:
+            return self._getErr(args, 'No identity specified')
+
+        trust_root = args.get('openid.trust_root')
+        tr = TrustRoot.parse(trust_root)
+        if tr is None:
+            return self._getErr(args, 'Malformed trust_root: %s' % trust_root)
+
+        return_to = args.get('openid.return_to')
+        if return_to is None:
+            return self._getErr(args, 'No return_to URL specified')
+
+        if not tr.validateURL(return_to):
+            return self._getErr(
+                args, 'return_to(%s) not valid against trust_root(%s)' % (
+                return_to, trust_root))
+
+        assoc_handle = args.get('openid.assoc_handle')
+        mode = args.get('openid.mode')
+
+        if not authorized:
+            if mode == 'checkid_immediate':
+                nargs = dict(args)
+                nargs['openid.mode'] = 'checkid_setup'
+                return REDIRECT, oidutil.appendArgs(self.url, nargs)
+
+            elif mode == 'checkid_setup':
+                ret = oidutil.appendArgs(self.url, args)
+                can = oidutil.appendArgs(return_to, {'openid.mode': 'cancel'})
+
+                return DO_AUTH, (ret, can)
+
+            else:
+                return self._getErr(
+                    args, 'open.mode (%r) not understood' % mode)
+
+        reply = {
+            'openid.mode': 'id_res',
+            'openid.return_to': return_to,
+            'openid.identity': identity,
+            }
+
+        if assoc_handle:
+            assoc = self.store.getAssociation(self.normal, assoc_handle)
+
+            # fall back to dumb mode if assoc_handle not found,
+            # and send the consumer an invalidate_handle message
+            if assoc is None or assoc.expiresIn <= 0:
+                if assoc is not None and assoc.expiresIn <= 0:
+                    self.store.removeAssociation(self.normal, assoc.handle)
+                
+                assoc = self._createAssociation('HMAC-SHA1')
+                self.store.storeAssociation(self.dumb, assoc)
+                reply['openid.invalidate_handle'] = assoc_handle
+        else:
+            assoc = self._createAssociation('HMAC-SHA1')
+            self.store.storeAssociation(self.dumb, assoc)
+
+        reply['openid.assoc_handle'] = assoc.handle
+
+        assoc.addSignature(_signed_fields, reply)
+
+        return REDIRECT, oidutil.appendArgs(return_to, reply)
 
     def processPost(self, args):
         """
@@ -372,5 +463,122 @@ class OpenIDServer(object):
 
         @rtype: (C{str}, C{str})
         """
-        return self.impl.processPost(args)
+        try:
+            mode = args['openid.mode']
+            if mode == 'associate':
+                return self._associate(args)
+            elif mode == 'check_authentication':
+                return self._checkAuth(args)
+            else:
+                return self._postErr('Unrecognized openid.mode (%r)' % mode)
+        except KeyError, e:
+            return self._postErr(
+                'Necessary openid argument (%r) missing.' % e[0])
+
+    def _createAssociation(self, assoc_type):
+        if assoc_type == 'HMAC-SHA1':
+            secret = cryptutil.getBytes(20)
+        else:
+            return None
+
+        uniq = oidutil.toBase64(cryptutil.getBytes(4))
+        handle = '{%s}{%x}{%s}' % (assoc_type, int(time.time()), uniq)
+
+        assoc = Association.fromExpiresIn(
+            self.SECRET_LIFETIME, handle, secret, assoc_type)
+
+        return assoc
+
+    def _associate(self, args):
+        reply = {}
+        assoc_type = args.get('openid.assoc_type', 'HMAC-SHA1')
+        assoc = self._createAssociation(assoc_type)
+
+        if assoc is None:
+            return self._postErr(
+                'unable to create an association for type %r' % assoc_type)
+        else:
+            self.store.storeAssociation(self.normal, assoc)
+
+        reply.update({
+            'assoc_type': 'HMAC-SHA1',
+            'assoc_handle': assoc.handle,
+            'expires_in': str(assoc.expiresIn),
+            })
+
+        session_type = args.get('openid.session_type')
+        if session_type:
+            if session_type == 'DH-SHA1':
+                p = args['openid.dh_modulus']
+                g = args['openid.dh_gen']
+                consumer_public = args['openid.dh_consumer_public']
+
+                dh = DiffieHellman.fromBase64(p, g)
+
+                cpub = cryptutil.base64ToLong(consumer_public)
+                mac_key = dh.xorSecret(cpub, assoc.secret)
+                spub = dh.createKeyExchange()
+
+                reply.update({
+                    'session_type': session_type,
+                    'dh_server_public': cryptutil.longToBase64(spub),
+                    'enc_mac_key': oidutil.toBase64(mac_key),
+                    })
+            else:
+                return self._postErr('session_type must be DH-SHA1')
+        else:
+            reply['mac_key'] = oidutil.toBase64(assoc.secret)
+
+        return OK, kvform.dictToKV(reply)
+
+    def _checkAuth(self, args):
+        assoc = self.store.getAssociation(
+            self.dumb, args['openid.assoc_handle'])
+
+        if assoc is None:
+            return self._postErr(
+                'no secret found for %r' % args['openid.assoc_handle'])
+
+        reply = {}
+        if assoc.expiresIn > 0:
+            dat = dict(args)
+            dat['openid.mode'] = 'id_res'
+
+            signed_fields = args['openid.signed'].strip().split(',')
+            v_sig = assoc.signDict(signed_fields, dat)
+
+            if v_sig == args['openid.sig']:
+                self.store.removeAssociation(
+                    self.normal, args['openid.assoc_handle'])
+                is_valid = 'true'
+
+                invalidate_handle = args.get('openid.invalidate_handle')
+                if invalidate_handle:
+                    if not self.store.getAssociation(self.normal,
+                                                     invalidate_handle):
+                        reply['invalidate_handle'] = invalidate_handle
+            else:
+                is_valid = 'false'
+
+        else:
+            self.store.removeAssociation(
+                self.dumb, args['openid.assoc_handle'])
+            is_valid = 'false'
+
+        reply['is_valid'] = is_valid
+        return OK, kvform.dictToKV(reply)
+
+    def _getErr(self, args, msg):
+        return_to = args.get('openid.return_to')
+        if return_to:
+            err = {
+                'openid.mode': 'error',
+                'openid.error': msg
+                }
+            return REDIRECT, oidutil.appendArgs(return_to, err)
+        else:
+            return ERROR, msg
+
+    def _postErr(self, msg):
+        return ERROR, kvform.dictToKV({'error': msg})
 
