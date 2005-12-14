000a8a622cb44cbbac4b660ef2ecfc4bbddce6a1
tailor <chowells@janrain.com>
2005-12-14 20:37:19 +0000

    [project @ Add more thorough testing of dh.py]

--- test/dh.py
+++ test/dh.py
@@ -1,3 +1,4 @@
+import os.path
 from openid.dh import DiffieHellman
 
 def test1():
@@ -13,5 +14,18 @@ def test():
     s2 = test1()
     assert s1 != s2
 
+def test_public():
+    f = file(os.path.join(os.path.dirname(__file__), 'dhpriv'))
+    dh = DiffieHellman()
+    try:
+        for line in f:
+            parts = line.strip().split(' ')
+            dh._setPrivate(long(parts[0]))
+
+            assert dh.public == long(parts[1])
+    finally:
+        f.close()
+
 if __name__ == '__main__':
     test()
+    test_public()
