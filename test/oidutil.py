7906f60fc77b322b7c6ffbfde5bcbf87499132bc
Josh Hoyt <josh@janrain.com>
2005-12-24 01:14:40 +0000

    [project @ Added more cases to appendArgs test]

--- test/oidutil.py
+++ test/oidutil.py
@@ -103,18 +103,77 @@ class AppendArgsTest(unittest.TestCase):
 def buildAppendTests():
     simple = 'http://www.example.com/'
     cases = [
-        ('empty list', (simple, []), simple),
-        ('empty dict', (simple, {}), simple),
-        ('one list', (simple, [('a', 'b')]), simple + '?a=b'),
-        ('one dict', (simple, {'a':'b'}), simple + '?a=b'),
+        ('empty list',
+         (simple, []),
+         simple),
+
+        ('empty dict',
+         (simple, {}),
+         simple),
+
+        ('one list',
+         (simple, [('a', 'b')]),
+         simple + '?a=b'),
+
+        ('one dict',
+         (simple, {'a':'b'}),
+         simple + '?a=b'),
+
         ('two list (same)',
-         (simple, [('a', 'b'), ('a', 'c')]), simple + '?a=b&a=c'),
-        ('two list', (simple, [('a', 'b'), ('b', 'c')]), simple + '?a=b&b=c'),
+         (simple, [('a', 'b'), ('a', 'c')]),
+         simple + '?a=b&a=c'),
+
+        ('two list',
+         (simple, [('a', 'b'), ('b', 'c')]),
+         simple + '?a=b&b=c'),
+
         ('two list (order)',
-         (simple, [('b', 'c'), ('a', 'b')]), simple + '?b=c&a=b'),
+         (simple, [('b', 'c'), ('a', 'b')]),
+         simple + '?b=c&a=b'),
+
         ('two dict (order)',
-         (simple, {'b':'c', 'a':'b'}), simple + '?a=b&b=c'),
-        ('escape', (simple, [('=', '=')]), simple + '?%3D=%3D'),
+         (simple, {'b':'c', 'a':'b'}),
+         simple + '?a=b&b=c'),
+
+        ('escape',
+         (simple, [('=', '=')]),
+         simple + '?%3D=%3D'),
+
+        ('escape (URL)',
+         (simple, [('this_url', simple)]),
+         simple + '?this_url=http%3A%2F%2Fwww.example.com%2F'),
+
+        ('use dots',
+         (simple, [('openid.stuff', 'bother')]),
+         simple + '?openid.stuff=bother'),
+
+        ('args exist (empty)',
+         (simple + '?stuff=bother', []),
+         simple + '?stuff=bother'),
+
+        ('args exist',
+         (simple + '?stuff=bother', [('ack', 'ack')]),
+         simple + '?stuff=bother&ack=ack'),
+
+        ('args exist',
+         (simple + '?stuff=bother', [('ack', 'ack')]),
+         simple + '?stuff=bother&ack=ack'),
+
+        ('args exist (dict)',
+         (simple + '?stuff=bother', {'ack': 'ack'}),
+         simple + '?stuff=bother&ack=ack'),
+
+        ('args exist (dict 2)',
+         (simple + '?stuff=bother', {'ack': 'ack', 'zebra':'lion'}),
+         simple + '?stuff=bother&ack=ack&zebra=lion'),
+
+        ('three args (dict)',
+         (simple, {'stuff': 'bother', 'ack': 'ack', 'zebra':'lion'}),
+         simple + '?ack=ack&stuff=bother&zebra=lion'),
+
+        ('three args (list)',
+         (simple, [('stuff', 'bother'), ('ack', 'ack'), ('zebra', 'lion')]),
+         simple + '?stuff=bother&ack=ack&zebra=lion'),
         ]
 
     tests = []
