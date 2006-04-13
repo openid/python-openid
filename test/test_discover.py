cb64ff7481793de6e5c0fde28144ee07d8b623ee
Josh Hoyt <josh@janrain.com>
2006-04-13 16:07:08 +0000

    [project @ Add datadriven module for data-driven test case helper code]

--- test/test_discover.py
+++ test/test_discover.py
@@ -1,4 +1,6 @@
+import sys
 import unittest
+import datadriven
 from urljr.fetchers import HTTPResponse
 from yadis.discover import DiscoveryFailure
 from openid.consumer import discover
@@ -13,36 +15,67 @@ class SimpleMockFetcher(object):
         assert url == self.url
         return HTTPResponse(url, self.status)
 
-class DidFetch(Exception):
-    pass
+class TestDiscoveryFailure(datadriven.DataDrivenTestCase):
+    cases = [
+        (None, 'http://network.error/'),
+        (404, 'http://not.found/'),
+        (400, 'http://bad.request/'),
+        (500, 'http://server.error/'),
+        ]
+
+    def __init__(self, url, status):
+        datadriven.DataDrivenTestCase.__init__(self, url)
+        self.url = url
+        self.status = status
+
+    def runTest(self):
+        fetcher = SimpleMockFetcher(self.status, self.url)
+        try:
+            discover.discover(self.url, fetcher)
+        except DiscoveryFailure, why:
+            self.failUnlessEqual(why.http_response.status, self.status)
+        else:
+            self.fail('Did not raise DiscoveryFailure')
 
 class ErrorRaisingFetcher(object):
+    def __init__(self, thing_to_raise):
+        self.thing_to_raise = thing_to_raise
+
     def fetch(self, url, body=None, headers=None):
-        raise DidFetch()
+        raise self.thing_to_raise
 
-class TestFetch(unittest.TestCase):
-    def test_badFetch(self):
-        cases = [
-            (None, 'http://network.error/'),
-            (404, 'http://not.found/'),
-            (400, 'http://bad.request/'),
-            (500, 'http://server.error/'),
-            ]
+class DidFetch(Exception): pass
 
-        for error_code, url in cases:
-            fetcher = SimpleMockFetcher(error_code, url)
-            try:
-                discover.discover(url, fetcher)
-            except DiscoveryFailure, why:
-                self.failUnlessEqual(why.http_response.status, error_code)
-            else:
-                self.fail('Did not raise DiscoveryFailure')
+class TestFetchException(datadriven.DataDrivenTestCase):
+    cases = [
+        (Exception(),),
+        (DidFetch(),),
+        (ValueError(),),
+        (RuntimeError(),),
+        ('oi!',),
+        ]
 
-    def test_fetchExc(self):
+    def __init__(self, exc):
+        datadriven.DataDrivenTestCase.__init__(self, repr(exc))
+        self.exc = exc
+
+    def runTest(self):
         """Make sure exceptions get passed through discover function
         from fetcher."""
-        fetcher = ErrorRaisingFetcher()
-        self.failUnlessRaises(DidFetch,
-                              discover.discover,
-                              'http://doesnt.matter/',
-                              fetcher)
+        fetcher = ErrorRaisingFetcher(self.exc)
+        try:
+            discover.discover('http://doesnt.matter/', fetcher)
+        except:
+            exc = sys.exc_info()[1]
+            if exc is None:
+                # str exception
+                self.failUnless(self.exc is sys.exc_info()[0])
+            else:
+                self.failUnless(self.exc is exc, exc)
+        else:
+            self.fail('Expected %r', self.exc)
+
+
+
+def pyUnitTests():
+    return datadriven.loadTests(__name__)
