61acfcffc2ac8c561fb741bea5683c1e8fec3764
tailor <chowells@janrain.com>
2006-08-11 22:59:09 +0000

    [project @ Handle XRDSErrors in discoverXRI]

--- openid/consumer/discover.py
+++ openid/consumer/discover.py
@@ -176,12 +176,16 @@ def discoverYadis(uri):
 
 
 def discoverXRI(iname):
-    services = xri.ProxyResolver().query(
-        iname, OpenIDServiceEndpoint.openid_type_uris)
     endpoints = []
-    flt = filters.mkFilter(OpenIDServiceEndpoint)
-    for service_element in services:
-        endpoints.extend(flt.getServiceEndpoints(iname, service_element))
+    try:
+        services = xri.ProxyResolver().query(
+            iname, OpenIDServiceEndpoint.openid_type_uris)
+        flt = filters.mkFilter(OpenIDServiceEndpoint)
+        for service_element in services:
+            endpoints.extend(flt.getServiceEndpoints(iname, service_element))
+    except XRDSError:
+        oidutil.log('xrds error on ' + iname)
+
     # FIXME: returned xri should probably be in some normal form
     return iname, endpoints
 
