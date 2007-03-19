1c9a141b4a2482cd21d1b0aa1f13e24a8ee5501b
tailor <cygnus@janrain.com>
2007-03-19 21:39:48 +0000

    [project @ Added initial Django example code tree]

---
+++ examples/djopenid/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls.defaults import *
+
+urlpatterns = patterns('',
+    # Example:
+    # (r'^djopenid/', include('djopenid.apps.foo.urls.foo')),
+
+    # Uncomment this for admin:
+#     (r'^admin/', include('django.contrib.admin.urls')),
+)
