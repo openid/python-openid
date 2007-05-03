cf4463d8008b0ef6b126e4c27088930134b72334
Josh Hoyt <josh@janrain.com>
2007-05-03 22:42:10 +0000

    [project @ Wrap long line]

--- openid/sreg.py
+++ openid/sreg.py
@@ -35,7 +35,8 @@ OpenID providers.
     namespace and XRD Type value
 """
 
-from openid.message import registerNamespaceAlias, NamespaceAliasRegistrationError
+from openid.message import registerNamespaceAlias, \
+     NamespaceAliasRegistrationError
 from openid.extension import Extension
 from openid import oidutil
 
