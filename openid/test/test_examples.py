d40dfd2f101b9b6e904d28b7b7ae2a8f477a70d4
Kevin Turner <kevin@janrain.com>
2007-06-20 23:22:08 +0000

    [project @ openid.test.test_examples: added to test examples/server.py.  Depends on Twill.]
    Super-hacky at the moment, needs work to reduce the number of private
    twill and mechanize attributes it abuses.

---
+++ openid/test/test_examples.py
@@ -0,0 +1,116 @@
+"Test some examples."
+
+import os.path, unittest, urllib2, sys, time
+from cStringIO import StringIO
+
+import twill.commands, twill.parse, twill.unit
+
+from openid.consumer.discover import \
+     OpenIDServiceEndpoint, OPENID_1_1_TYPE
+from openid.consumer.consumer import AuthRequest
+
+class TwillTest(twill.unit.TestInfo):
+    """Variant of twill.unit.TestInfo that runs scripts from strings,
+    not filename."""
+    def run_script(self):
+        time.sleep(self.sleep)
+        # twill.commands.go(self.get_url())
+        self.script(self)
+        
+
+def runExampleServer(host, port, data_path):
+    thisfile = os.path.abspath(sys.modules[__name__].__file__)
+    topDir = thisfile.rsplit(os.sep, 3)[0]
+    exampleDir = os.path.join(topDir, 'examples')
+    serverExample = os.path.join(exampleDir, 'server.py')
+    serverModule = {}
+    execfile(serverExample, serverModule)
+    serverMain = serverModule['main']
+
+    serverMain(host, port, data_path)
+
+# def scriptName(name):
+#     return os.path.join(os.path.dirname(sys.modules[__name__].__file__),
+#                         'twill', name)
+
+
+
+class RedirectHandler(urllib2.HTTPRedirectHandler):
+    def __init__(self):
+        self.redirectList = []
+
+    def redirect_request(self, req, fp, code, msg, headers, newurl):
+        self.redirectList.append(newurl)
+
+class TestServer(unittest.TestCase):
+    def setUp(self):
+        import twill
+        self.twillOutput = StringIO()
+        self.twillErr = StringIO()
+        twill.set_output(self.twillOutput)
+        twill.set_errout(self.twillErr)
+        self.server_port = 8080
+
+        # We need something to feed the server as a realm, but it needn't
+        # be reachable.  (Until we test realm verification.)
+        self.realm = 'http://127.0.0.1/%s' % (self.id(),)
+        self.return_to = self.realm + '/return_to'
+
+        twill.commands.reset_browser()
+
+    def runExampleServer(self):
+        # FIXME - make sure sstore starts clean.
+        runExampleServer('127.0.0.1', self.server_port, 'sstore')
+
+    def v1endpoint(self, port):
+        base = "http://127.0.0.1:%s" % (port,)
+        ep = OpenIDServiceEndpoint()
+        ep.claimed_id = base + "/id/bob"
+        ep.server_url = base + "/openidserver"
+        ep.type_uris = [OPENID_1_1_TYPE]
+        return ep
+        
+    # TODO: test discovery
+
+    def test_checkidv1(self):
+        ti = TwillTest(self.twill_checkidv1, self.runExampleServer,
+                       self.server_port, sleep=0.2)
+        twill.unit.run_test(ti)
+        
+        if self.twillErr.getvalue():
+            self.fail(self.twillErr.getvalue())
+
+        # self.fail(self.twillOutput.getvalue())
+
+    def twill_checkidv1(self, twillInfo):
+        endpoint = self.v1endpoint(self.server_port)
+        authreq = AuthRequest(endpoint, assoc=None)
+        url = authreq.redirectURL(self.realm, self.return_to)
+
+        c = twill.commands
+
+        redirectHandler = RedirectHandler()
+        
+        try:
+            c.go(url)
+            c.get_browser()._browser._replace_handler("_redirect",
+                                                      redirectHandler)
+            c.submit("yes")
+            c.code(302)
+            self.failUnlessEqual(len(redirectHandler.redirectList), 1,
+                                 redirectHandler.redirectList)
+            finalURL = redirectHandler.redirectList[0]
+            self.failUnless('openid.mode=id_res' in finalURL, finalURL)
+            self.failUnless('openid.identity=' in finalURL, finalURL)
+        except twill.commands.TwillAssertionError, e:
+            self.fail(RedirectHandler.wtfList)
+            b = c.get_browser()
+            msg = '%s\nFinal page:\n%s' % (
+                str(e), b.get_html())
+            self.fail(msg)
+
+    def tearDown(self):
+        twill.set_output(None)
+        twill.set_errout(None)
+
+unittest.main()
