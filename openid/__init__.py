779ce43519ecfcfc2d1637d682bf503b913e2b32
tailor <cygnus@janrain.com>
2008-06-05 23:56:50 +0000

    [project @ Change __version__ to 2.2.0]

--- openid/__init__.py
+++ openid/__init__.py
@@ -23,7 +23,7 @@ module.
     and limitations under the License.
 """
 
-__version__ = '[library version:2.1.1]'[17:-1]
+__version__ = '[library version:2.2.0]'[17:-1]
 
 __all__ = [
     'association',
