6075706658e23af854bc73eb627b309b740b2e2c
http://j3h.us/ <http://j3h.us/@lillialexis.local>
2008-06-10 23:42:22 +0000

    [project @ Reject URIs with illegal characters in urinorm]

--- openid/urinorm.py
+++ openid/urinorm.py
@@ -4,6 +4,15 @@ import re
 uri_pattern = r'^(([^:/?#]+):)?(//([^/?#]*))?([^?#]*)(\?([^#]*))?(#(.*))?'
 uri_re = re.compile(uri_pattern)
 
+# gen-delims  = ":" / "/" / "?" / "#" / "[" / "]" / "@"
+#
+# sub-delims  = "!" / "$" / "&" / "'" / "(" / ")"
+#                  / "*" / "+" / "," / ";" / "="
+#
+# unreserved  = ALPHA / DIGIT / "-" / "." / "_" / "~"
+
+uri_illegal_char_re = re.compile(
+    "[^-A-Za-z0-9:/?#[\]@!$&'()*+,;=._~%]", re.UNICODE)
 
 authority_pattern = r'^([^@]*@)?([^:]*)(:.*)?'
 authority_re = re.compile(authority_pattern)
@@ -130,6 +139,11 @@ def remove_dot_segments(path):
 def urinorm(uri):
     if isinstance(uri, unicode):
         uri = _escapeme_re.sub(_pct_escape_unicode, uri).encode('ascii')
+
+    illegal_mo = uri_illegal_char_re.search(uri)
+    if illegal_mo:
+        raise ValueError('Illegal characters in URI: %r at position %s' %
+                         (illegal_mo.group(), illegal_mo.start()))
 
     uri_mo = uri_re.match(uri)
 
