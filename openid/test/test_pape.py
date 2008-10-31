d527657eaa6a49d4b50d25fff8d7bec4b77e9994
Kevin Turner <kevin@janrain.com>
2008-10-31 23:56:29 +0000

    [project @ python 2.3 does not have assertTrue]

--- openid/test/test_pape.py
+++ openid/test/test_pape.py
@@ -6,4 +6,4 @@ import unittest
 class PapeImportTestCase(unittest.TestCase):
     def test_version(self):
         from openid.extensions.draft import pape5
-        self.assertTrue(pape is pape5)
+        self.assert_(pape is pape5)
--- openid/test/test_pape_draft5.py
+++ openid/test/test_pape_draft5.py
@@ -53,7 +53,7 @@ class PapeRequestTestCase(unittest.TestCase):
         # alias is None; we expect a new one to be generated.
         uri = 'http://another.example.com/'
         self.req.addAuthLevel(uri)
-        self.assertTrue(uri in self.req.auth_level_aliases.values())
+        self.assert_(uri in self.req.auth_level_aliases.values())
 
         # We don't expect a new alias to be generated if one already
         # exists.
