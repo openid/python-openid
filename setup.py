c1c866a569813811df0a3378b38b672a282b9a62
Grant Monroe <grant@janrain.com>
2010-06-18 08:05:07 +0800

    Fix download_url in setup.py

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ and support for a variety of storage back-ends.''',
     # license=getLicense(),
     author='JanRain',
     author_email='openid@janrain.com',
-    download_url='http://github.com/openid/python-openid/tarball/master',
+    download_url='http://github.com/openid/python-openid/tarball/%s' % (version,),
     classifiers=[
     "Development Status :: 5 - Production/Stable",
     "Environment :: Web Environment",
