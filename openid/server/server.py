openid/python-openid

commit ff4a41754d68791ae0df3593661f3e29583e7b7f

url limit is present in openid 1 and not on openid 2

openid/server/server.py
@@ -128,7 +128,7 @@ from openid.store.nonce import mkNonce
 from openid.server.trustroot import TrustRoot, verifyReturnTo
 from openid.association import Association, default_negotiator, getSecretSize
 from openid.message import Message, InvalidOpenIDNamespace, \
-     OPENID_NS, OPENID2_NS, IDENTIFIER_SELECT, OPENID1_URL_LIMIT
+     OPENID_NS, OPENID1_NS, OPENID2_NS, IDENTIFIER_SELECT, OPENID1_URL_LIMIT
 from openid.urinorm import urinorm
 
 HTTP_OK = 200
@@ -1043,7 +1043,7 @@ class OpenIDResponse(object):
         @change: 2.1.0 added the ENCODE_HTML_FORM response.
         """
         if self.request.mode in BROWSER_REQUEST_MODES:
-            if self.fields.getOpenIDNamespace() == OPENID2_NS and \
+            if self.fields.getOpenIDNamespace() == OPENID1_NS and \
                len(self.encodeToURL()) > OPENID1_URL_LIMIT:
                 return ENCODE_HTML_FORM
             else:
@@ -1719,7 +1719,7 @@ class ProtocolError(Exception):
             displayed to the user.
         """
         if self.hasReturnTo():
-            if self.openid_message.getOpenIDNamespace() == OPENID2_NS and \
+            if self.openid_message.getOpenIDNamespace() == OPENID1_NS and \
                len(self.encodeToURL()) > OPENID1_URL_LIMIT:
                 return ENCODE_HTML_FORM
             else:

commit 48c6dbfb312fc49dfdebdedbe17b16813528b8ed

Fix bug introduced in ff4a417

openid/server/server.py
@@ -1043,8 +1043,9 @@ class OpenIDResponse(object):
         @change: 2.1.0 added the ENCODE_HTML_FORM response.
         """
         if self.request.mode in BROWSER_REQUEST_MODES:
-            if self.fields.isOpenID1() and \
+            if self.fields.isOpenID2() and \
                len(self.encodeToURL()) > OPENID1_URL_LIMIT:
+                # Message can be encoded as HTML form only if it's OpenID 2.0.
                 return ENCODE_HTML_FORM
             else:
                 return ENCODE_URL
@@ -1719,8 +1720,9 @@ class ProtocolError(Exception):
             displayed to the user.
         """
         if self.hasReturnTo():
-            if self.openid_message.isOpenID1() and \
+            if self.openid_message.isOpenID2() and \
                len(self.encodeToURL()) > OPENID1_URL_LIMIT:
+                # Message can be encoded as HTML form only if it's OpenID 2.0.
                 return ENCODE_HTML_FORM
             else:
                 return ENCODE_URL