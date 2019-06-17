2b1e133ed17073e41639987053c8ca0e030fedb6
Vlastimil Zíma <vlastimil.zima@nic.cz>
2019-06-17 10:50:09 +0200

    Fix isort

--- openid/consumer/__init__.py
+++ openid/consumer/__init__.py
@@ -2,5 +2,6 @@
 This package contains the portions of the library used only when
 implementing an OpenID consumer.
 """
+from __future__ import unicode_literals
 
 __all__ = ['consumer', 'discover']
--- openid/server/__init__.py
+++ openid/server/__init__.py
@@ -2,5 +2,6 @@
 This package contains the portions of the library used only when
 implementing an OpenID server.  See L{openid.server.server}.
 """
+from __future__ import unicode_literals
 
 __all__ = ['server', 'trustroot']
